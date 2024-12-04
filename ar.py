6bc5d95495a04f037f47a4ac5a9d6e3861341077
DEATHPIXEL-PIXEL <zayaankhan337@gmail.com>
2024-12-04 17:23:24 +0530

    first commit

---
+++ ar.py
@@ -0,0 +1,10 @@
+
+
+pi = 3.14159  
+
+radius = float(input("Enter the radius of the circle: "))
+circumference = 2 * pi * radius
+area = pi * radius ** 2
+
+print("circumfrence = " ,  circumference)
+print("Area = "  , area)
